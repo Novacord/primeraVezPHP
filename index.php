59a3b399734830fa55d60ae398658d5189457062
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-05-29 17:23:11 -0500

    feat: :sparkles: Tipos de datos
    Enteros, Punto flotante, Cadenas de texto, Booleanos, Arreglos

--- index.php
+++ index.php
@@ -49,6 +49,66 @@
  */
 
  define('SALUDO','Hola');
- echo SALUDO
+ echo SALUDO;
+
+/**
+ ** Tipos de datos
+ */
+
+/**
+ **Enteros (int): se utilizan para almacenar números enteros sin decimales.
+ */
+
+/**
+ * @param $numeroInt guarda el numero entero 2
+ */
+
+ $numeroInt = 2;
+ echo $numeroInt;
+
+/**
+ **Punto flotante (float): se utilizan para almacenar números con decimales.
+ */
+
+/**
+ * @param $numeroFloat guarda el numero flotante 3.6
+ */
+
+ $numeroFloat = 3.6;
+ echo $numeroFloat;
+
+/**
+ **Cadenas de texto (string): se utilizan para almacenar texto y caracteres.
+ */
+
+/**
+ * @param $string guarda el texto de tipo string
+ */
+
+ $string = 'text';
+ echo $string;
+
+/**
+ * * Booleanos (bool): se utilizan para almacenar valores de verdad o falsedad, que se representan por true o false.
+ */
+
+/**
+ * @param $verdadero me esta guardando true lo que mi imprime 1 porque es booleano
+ */
+
+$verdadero = true;
+echo $verdadero;
+
+/**
+ * *Arreglos (array): se utilizan para almacenar una colección de datos, que pueden ser de diferentes tipos.
+ */
+
+/**
+ * @param $arreglo esta guardando es una lista o array los numero 0,1
+ */
+
+ $arreglo = [0,1];
+
+ echo $arreglo[0];
 
 ?>
