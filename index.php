bcefc82a45cb8e937851b8e0c348cce48f79dbb6
JoseDanielNovaMuñozCAMPUS <josedanielnova2027@gmail.com>
2023-05-29 17:01:38 -0500

    feat: :sparkles: Funciones de salida en PHP
    como poder escribir en el documento en PHP

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+/**
+ ** Funciones de salida en PHP
+ */
+
+ /**
+  * @param echo 'hola mundo', escribe en el documento el texto escrito en este caso hola mundo
+  */
+
+  echo 'hola mundo';
 
 
+ /**
+  * @param printf ('hola mundo'), escribe en el documento el texto escrito en este caso hola mundo
+  */
 
+    printf('hola Mundo');
+
+ /**
+  * @param sprintf ('hola mundo'), te arma el texto escrito en este caso hola mundo y se puede guardar en una variable
+  */
+
+  $texto = 'mundo';
+  $mensaje = sprintf('hola',$texto);
+  echo $mensaje;
 ?>
